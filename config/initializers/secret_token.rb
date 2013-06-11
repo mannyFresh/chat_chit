2456bb9fbc11d742e52faff2a2927755f85746d8
mannyFresh <marcs@ccs.neu.edu>
2013-06-10 23:29:14 -0400

    newapp usersignup Tests pass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1dcf9dcfcb8645db766dc30462cba4e6a2c2ba0d16c04956550d331d686d222bd38c0f9392cd62dac3a5793dce7bd0c80358340aac80138a704f2cd55f95f179'
